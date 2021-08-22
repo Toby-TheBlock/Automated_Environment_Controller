261cc9de955e7539c4f2f5a4788444a757c9dfaf
Toby-TheBlock <tobiasgunther@hotmail.no>
2021-08-23 00:17:42 +0200

    Started the creation of a webserver with a new ASP.NET Core application template.

---
+++ Automated_Enviroment_Controller/Pages/Configuration.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Automated_Enviroment_Controller.Pages
+{
+    public class ConfigurationModel : PageModel
+    {
+        public int Test()
+        {
+            return 1;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
